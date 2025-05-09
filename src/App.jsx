7a34c62f9a55c3dda3b35582e38e95c16f01dcf6
gyeongsangseaman <sadx302@gmail.com>
2025-05-09 20:00:32 +0900

    타이머 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ItemList from "./ItemList";
+import TimerSwitch from "./TimerSwitch";
 
 const App = () => {
-  return <ItemList />;
+  return <TimerSwitch />;
 };
 
 export default App;
